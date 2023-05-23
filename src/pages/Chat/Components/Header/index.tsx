e82b8c151ddbe06fcef3d19ccb78a23a3fdc08f7
Anderson Lima <andersonlimahw@gmail.com>
2023-05-22 22:39:42 -0300

    🎨 [style] [SendMessage] - Input de envio de msg com render condicional

--- src/pages/Chat/Components/Header/index.tsx
+++ src/pages/Chat/Components/Header/index.tsx
@@ -4,16 +4,16 @@ import { IUser, useChat } from "../../../../store/hooks/use-chat-store";
 export const Header = () => {
     const selectedContact = useChat((state: any) => state.selectedContact) as IUser;
     return (
-        <div className="py-2 px-3 bg-grey-lighter flex flex-row justify-between items-center">
-            <div className="flex items-center">
+        <div className="py-2 px-3 bg-grey-lighter flex flex-row justify-start items-start">
+            <div className="flex items-start">
                 <div>
                     <img className="w-10 h-10 rounded-full" src={selectedContact?.photoURL} />
                 </div>
-                <div className="ml-4">
-                    <p className="text-grey-darkest">
+                <div className="text-left ml-4">
+                    <p className="text-zinc-300 font-semibold">
                         {selectedContact?.displayName}
                     </p>
-                    <p className="text-grey-darker text-xs mt-1">
+                    <p className="text-zinc-400 text-xs mt-1">
                         {selectedContact?.email}
                     </p>
                 </div>
--- src/pages/Chat/Components/SendMessage/index.tsx
+++ src/pages/Chat/Components/SendMessage/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { PaperPlaneRight  } from "@phosphor-icons/react";
+import { PaperPlaneRight } from "@phosphor-icons/react";
 import { create } from "../../../../services/firebase";
 import { IState, useChat, IUser } from '../../../../store/hooks/use-chat-store';
 import { EMessageStatus } from "../Message";
@@ -9,15 +9,15 @@ export const SendMessageInput = () => {
     const [message, setMessage] = useState('');
     const chatStore = useChat((state: any) => state);
     const { user, selectedContact } = chatStore as IState;
-    const { uid, email, displayName, photoURL  } = user as IUser;
+    const { uid, email, displayName, photoURL } = user as IUser;
 
     const sendMessage = async (message: string) => {
         return create({
             collectionName: 'chat',
-            payload: {                
+            payload: {
                 message: message,
                 to: selectedContact?.email || 'andersonlimadeveloper@gmail.com',
-                from: email,                
+                from: email,
                 displayName,
                 photoURL,
                 uid: uid,
@@ -26,7 +26,8 @@ export const SendMessageInput = () => {
         });
     };
 
-    const handleSendMessage = async () => {
+    const handleSendMessage = async (event: any) => {
+        event.preventDefault();
         if (message.trim() !== '') {
             await sendMessage(message);
             setMessage('');
@@ -34,18 +35,24 @@ export const SendMessageInput = () => {
     };
 
     return (
-        <div className="bg-grey-lighter px-4 py-4 flex items-center">
-            <div className="flex-1 mx-4">
-                <input
-                    className="w-full border rounded px-2 py-2"
-                    type="text"
-                    value={message}
-                    onChange={(e) => setMessage(e.target.value)}
-                />
-            </div>
-            <div>
-                <PaperPlaneRight onClick={() => handleSendMessage()} size={24} color="#fff" weight="fill" />
-            </div>
+        <div className="bg-grey-lighter px-4 py-4 flex items-start">
+            <form className="flex-1 flex-row mx-4 items-baseline" onSubmit={handleSendMessage}>
+                <div className="flex-inline">
+                    <input
+                        className="w-[94%] border-t-zinc-400 rounded px-2 py-2 flex-inline"
+                        type="text"
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                    />
+                    {
+                        message && message.length > 1 && (
+                            <span className="inline-block absolute ml-2 pt-1 items-center justify-items-center">
+                                <PaperPlaneRight onClick={handleSendMessage} size={24} color="#fff" weight="fill" />
+                            </span>
+                        )
+                    }
+                </div>
+            </form>
         </div>
     )
 }
